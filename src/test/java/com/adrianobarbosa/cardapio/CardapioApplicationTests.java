5a960140f76a1a6264b3cbe92bfb295afe74f0ee
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-10-09 09:31:14 -0300

    commit inicial

---
+++ src/test/java/com/adrianobarbosa/cardapio/CardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrianobarbosa.cardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
