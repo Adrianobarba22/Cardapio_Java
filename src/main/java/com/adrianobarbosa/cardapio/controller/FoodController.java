9f175e2ac6331e65796fe8c43db74f9f24fc8cb1
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-12-15 16:42:08 -0300

    commit final do  backend

--- src/main/java/com/adrianobarbosa/cardapio/controller/FoodController.java
+++ src/main/java/com/adrianobarbosa/cardapio/controller/FoodController.java
@@ -1,14 +1,36 @@
 package com.adrianobarbosa.cardapio.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.adrianobarbosa.cardapio.food.Food;
+import com.adrianobarbosa.cardapio.food.FoodRepository;
+import com.adrianobarbosa.cardapio.food.FoodRequestDTO;
+import com.adrianobarbosa.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("food")
 public class FoodController {
-    @GetMapping
-    public void getAll(){
 
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
     }
 }
---
+++ src/main/java/com/adrianobarbosa/cardapio/food/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.adrianobarbosa.cardapio.food;
+
+
+public record FoodRequestDTO(String title, String image, Integer price) {
+}
